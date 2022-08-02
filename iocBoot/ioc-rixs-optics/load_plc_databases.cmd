de1819a4e4194055b9f69ff9f79aa92dbca56d93
Zachary Lentz <zlentz@slac.stanford.edu>
2022-08-02 14:30:55 -0700

    BLD: rebuild IOC

--- iocBoot/ioc-rixs-optics/load_plc_databases.cmd
+++ iocBoot/ioc-rixs-optics/load_plc_databases.cmd
@@ -1,1 +1,3 @@
-dbLoadRecords("rixs_optics.db", "PORT=ASYN_PLC,PREFIX=PLC:RIXS:OPTICS:,IOCNAME=$(IOC),IOC=$(IOC)")
+# Total records: 5127
+callbackSetQueueSize(12254)
+dbLoadRecords("rixs_optics.db", "PORT=ASYN_PLC,PREFIX=PLC:RIX:OPTICS:,IOCNAME=$(IOC),IOC=$(IOC)")
