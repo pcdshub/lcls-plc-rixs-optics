74b0f1a965a143cd9b840f14deb1ca8daa9f2674
Zachary Lentz <zlentz@slac.stanford.edu>
2022-08-02 18:09:45 -0700

    BLD: rebuild without write PVs for DAQ encoder debug

--- iocBoot/ioc-rixs-optics/load_plc_databases.cmd
+++ iocBoot/ioc-rixs-optics/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 5127
-callbackSetQueueSize(12254)
+# Total records: 5123
+callbackSetQueueSize(12246)
 dbLoadRecords("rixs_optics.db", "PORT=ASYN_PLC,PREFIX=PLC:RIX:OPTICS:,IOCNAME=$(IOC),IOC=$(IOC)")
